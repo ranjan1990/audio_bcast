fedcc7075c41c1ab275653070971d2bafd58a206
root <ranjan.rbc@gmail.com>
2015-11-19 00:43:56 +0530

    commited

---
+++ cap_audio.sh
@@ -0,0 +1,6 @@
+
+
+arecord -f cd -t wav -d 5 |lame -r - out1.mp3
+arecord -f u8 -d 5 -t wav x1.wav
+arecord -f u8 -d 5 -t wav x2.wav
+#arecord -f dat -d 2 -t voc x2.wav
---
+++ play.sh
@@ -0,0 +1,6 @@
+
+aplay -f U8 x1.wav
+
+aplay -f U8 x2.wav
+
+
